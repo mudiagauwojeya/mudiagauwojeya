adb54dbe3c30f9fb9d2a59b949fe69886447888a
Mudiaga Uwojeya <moody.mudiaga@outlook.com>
2022-02-14 13:48:46 +0100

    :arrow_up_small: Update classnames in App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import classes from "./App.module.scss";
 function App() {
 	return (
 		<div className={classes.App}>
-			<header className={classes.App - header}>
+			<header className={classes.Header}>
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
 				<a
-					className={classes.App - link}
+					className={classes.Link}
 					href="https://reactjs.org"
 					target="_blank"
 					rel="noopener noreferrer"
